bc6921d5a1b48ac506cb6ef2c8d9e3a9e30250b9
Kavita <kavita.jangir@abjima.com>
2023-05-15 18:48:08 +0530

    Assignment

---
+++ Assignment/Data/ApiDbContex.cs
@@ -0,0 +1,14 @@
+﻿using Assignment.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment.Data
+{
+    public class ApiDbContex:DbContext
+    {
+        public ApiDbContex(DbContextOptions<ApiDbContex> options) : base(options)
+        {
+
+        }
+        public DbSet<Students> Students { get; set; }
+    }
+}
---
+++ Assignment/Models/Students.cs
@@ -0,0 +1,17 @@
+﻿namespace Assignment.Models
+{
+    public class Students
+    {
+        public int id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string? FamilyName { get; set; }
+
+        public string? Address { get; set; }
+
+        public long? ContactNumber { get; set; }
+
+
+    }
+}
